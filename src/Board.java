622a4cd835562d6790c2d00e41e8a67dba6339cd
hazirahka <zirakanuar@gmail.com>
2019-11-30 18:25:48 +0000

    1st commit

---
+++ src/Board.java
@@ -0,0 +1,47 @@
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Graphics;
+import javax.swing.Timer;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import javax.swing.ImageIcon;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Board extends JPanel implements ActionListener {
+
+    private final int B_WIDTH = 800;
+    private final int B_HEIGHT = 800;
+    private final int DOT_SIZE = 25;
+    private final int ALL_DOTS = 900;
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
+
+    private final int x[] = new int[ALL_DOTS];
+    private final int y[] = new int[ALL_DOTS];
+
+    private int dots;
+    private int apple_x;
+    private int apple_y;
+
+    private boolean leftArrow = false;
+    private boolean rightArrow = true;
+    private boolean upArrow = false;
+    private boolean downArrow = false;
+    private boolean inGame = true;
+
+    private Timer timer;
+    private Image body;
+    private Image apple;
+    private Image head;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+}
