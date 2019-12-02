7bb7e21af6c1828b74602382824fe67ac3a3a1f5
hazirahka <zirakanuar@gmail.com>
2019-12-02 01:06:31 +0000

    17th commit

--- src/AbsolemGUI.java
+++ src/AbsolemGUI.java
@@ -6,24 +6,15 @@ public class AbsolemGUI extends JFrame implements ActionListener {
     private JMenuBar dropMenu;
     private JMenu file;
     private JMenuItem newGame;
-    private JMenuItem loadGame;
-    private JMenuItem saveGame;
     private JMenuItem backMenu;
-    private Board Board;
 
     public AbsolemGUI() {
         this.newGame = new JMenuItem("New Game");
-        this.loadGame = new JMenuItem("Load Game");
-        this.saveGame = new JMenuItem("Save Game");
         this.backMenu = new JMenuItem("Back To Menu");
         this.newGame.addActionListener(this);
-        this.loadGame.addActionListener(this);
-        this.saveGame.addActionListener(this);
         this.backMenu.addActionListener(this);
         this.file = new JMenu("File");
         this.file.add(this.newGame);
-        this.file.add(this.saveGame);
-        this.file.add(this.loadGame);
         this.file.add(this.backMenu);
         this.dropMenu = new JMenuBar();
         this.dropMenu.add(this.file);
@@ -51,32 +42,6 @@ public class AbsolemGUI extends JFrame implements ActionListener {
         {
             AbsolemGUI absolem = new AbsolemGUI();
             this.dispose();
-        }
-
-        if (e.getSource() == this.saveGame)
-        {
-            /*try{
-                FileOutputStream fos = new FileOutputStream("absolem_data.ser");
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
-                oos.writeObject(AbsolemGUI.this);
-                oos.close();
-                fos.close();
-                JOptionPane.showMessageDialog(null, "Game is successfully saved!","Saved Game!",JOptionPane.INFORMATION_MESSAGE);
-            }catch (IOException ex){
-                ex.printStackTrace();
-            }*/
-        }
-
-        if (e.getSource() == this.loadGame)
-        {
-            /*try {
-                //Set the current Connect four object references and values equal to the previously saved games values
-                ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("absolem_data.ser"));
-                AbsolemGUI previousGame = (AbsolemGUI) objectInputStream.readObject();
-                AbsolemGUI.this.Board = previousGame.Board;
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }*/
         }
 
         if (e.getSource() == this.backMenu)
